72308c15430a57ee5982f7483e2340cccd33d380
tomutanyi <tomutanyi@gmail.com>
2023-08-07 02:27:09 +0300

    3 out of 4 passing

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData (userName , userEmail){
+    const curl = 
+    {
+    name: userName,
+    email: userEmail,
+        
+    }
+    
+   const poststh = 
+   {
+        method: "POST",
+        body: JSON.stringify(curl),
+        headers: {
+            "Content-type":"application/json",
+            accept:"application/json"
+        }
+    
+    }
+    return fetch("http://localhost:3000/users" , poststh)
+    .then(function (res) 
+    {
+        return res.json();
+    })
+    .then(function(curl) 
+    {
+        // return curl;
+        document.querySelector("body").textContent = curl.id 
+    })
+    .catch((err) => 
+    {
+        document.querySelector("body").textContent = err.message;
+      });
+
+}
